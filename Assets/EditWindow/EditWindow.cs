9ebf5edd62dbb18c66cd0c601ab367b4e10178ac
GermanWithSherman <german.with.sherman@gmail.com>
2020-06-16 07:25:57 +0200

    Test

--- Assets/EditWindow/EditWindow.cs
+++ Assets/EditWindow/EditWindow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,5 +15,30 @@ public class EditWindow : MonoBehaviour
     public void show(SubLocation subLocation)
     {
         show();
+
+        WidgetContainer.childrenDestroyAll();
+
+        _e = Instantiate(EditWidgetText,WidgetContainer);
+        _e.setCurrentValue(subLocation.TexturePath.value());
+
+        Canvas.ForceUpdateCanvases();
+    }
+}
+
+
+
+public class Unterricht
+{
+    
+    //Diesen Teil fassen SuS nicht an
+    private void WichtigeFunktion()
+    {
+        //Sehr wichtiger Code
+    }
+
+
+    public void Beispielprogramm()
+    {
+        //Hier wird von SuS programmiert
     }
 }
